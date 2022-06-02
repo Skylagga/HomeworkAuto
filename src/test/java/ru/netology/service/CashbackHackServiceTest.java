d44fced468aada988112a5a4c35540bb83b5bec9
Skylagga <mike4797@icloud.com>
2022-06-02 13:59:27 +0300

    Junit4 added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,39 +1,39 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import static org.junit.Test.*;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemain() {
         int boundary = 900;
         int expected = 100;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(boundary);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
 
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemain2() {
         int boundary = 1100;
         int expected = 900;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(boundary);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemain3() {
-        int boundary = 1999;
-        int expected = 1;
+        int boundary = 1000;
+        int expected = 0;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(boundary);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
 
     }
 
